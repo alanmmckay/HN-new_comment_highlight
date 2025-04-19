83126dcb04bd3f615d025a1e2852e9a2cefe9573
Alan McKay <51932760+alanmmckay@users.noreply.github.com>
2025-04-19 13:12:40 -0500

    Implemented logic to remove items from local storage after a month has passed. Fixed a logical error pertaining to recognizing primary posts that have no comments. Moved global logic into a main function.

--- content.js
+++ content.js
@@ -1,10 +1,22 @@
-async function print_val(val){
+async function print_val(val){ //used for asynchronous debugging
     console.log(val);
 }
 
+// --- Functions used for navigating and retreiving content of primary post:
 function get_subline_span(){
     let main_post = document.getElementsByClassName("fatitem")[0];
     return main_post.getElementsByClassName("subline")[0];
+}
+
+function get_primary_timestamp(){
+    let subline = get_subline_span();
+    let age_span = subline.getElementsByClassName("age")[0];
+    if(age_span){
+        let age_str = age_span.getAttribute("title");
+        let timestamp_str = age_str.split(/[ ,]+/)[1];
+        let timestamp_num = Number(timestamp_str);
+        return timestamp_num;
+    }
 }
 
 function get_count_anchor(){
@@ -28,6 +40,8 @@ function get_comment_count(){
     }
     return count;
 }
+// --- --- --- --- --- //
+
 
 async function store_data(){
     let max = 0;
@@ -43,9 +57,30 @@ async function store_data(){
             }
         }
     }
-    let count = get_comment_count();
+    let count = 0;
+    if(max == 0){
+        max = get_primary_timestamp();
+    }else{
+        count = get_comment_count();
+    }
     let key = "HN_timestamp_" + location.href;
     return chrome.storage.local.set({[key]: {"max":max,"count":count}});
+}
+
+async function clean_data(){
+    let now = Math.floor((new Date()).getTime() / 1000);
+    let keys = await chrome.storage.local.getKeys();
+    for( let key of keys){
+        if(key.toString().substr(0,13) == "HN_timestamp_"){
+            let entry = await chrome.storage.local.get([key]);
+            let timestamp = entry[key]['max'];
+            if( (timestamp + (4*604800)) < now ){
+                console.log(key);
+                console.log('here');
+                chrome.storage.local.remove([key]);
+            }
+        }
+    }
 }
 
 async function get_HN_item(){
@@ -62,56 +97,55 @@ function isEmpty(obj){
     return true;
 }
 
-get_HN_item().then( function(old_HN_item){
-    //console.log(old_HN_item);
-    if(!isEmpty(old_HN_item)){
-        let new_count = get_comment_count();
-        let key = "HN_timestamp_" + location.href;
-        if(new_count > old_HN_item[key]["count"]){
+function HN_nph_main(){
+    get_HN_item().then( function(old_HN_item){
+        //console.log(old_HN_item);
+        if(!isEmpty(old_HN_item)){
+            let new_count = get_comment_count();
+            let key = "HN_timestamp_" + location.href;
+            if(new_count > old_HN_item[key]["count"]){
 
-            let diff_count = (new_count - old_HN_item[key]["count"]).toString();
+                let diff_count = (new_count - old_HN_item[key]["count"]).toString();
 
-            let posts = document.getElementsByClassName("comtr");
-            let count = 0;
+                let posts = document.getElementsByClassName("comtr");
+                let count = 0;
 
-            for( let post of posts){
-                let age_span = post.getElementsByClassName("age")[0];
-                if(age_span){
-                    let age_str = age_span.getAttribute("title");
-                    let timestamp_str = age_str.split(/[ ,]+/)[1];
-                    let timestamp_num = Number(timestamp_str);
-                    if(timestamp_num > old_HN_item[key]["max"]){
-                        count += 1;
-                        let anchor = age_span.getElementsByTagName("a")[0];
-                        age_span.id = "np_"+count.toString();
-                        anchor.style['color'] = '#b37400';
-                        anchor.style['font-weight'] = 'bold';
-                        post.style['background-color'] = '#e6e6e6';
+                for( let post of posts){
+                    let age_span = post.getElementsByClassName("age")[0];
+                    if(age_span){
+                        let age_str = age_span.getAttribute("title");
+                        let timestamp_str = age_str.split(/[ ,]+/)[1];
+                        let timestamp_num = Number(timestamp_str);
+                        if(timestamp_num > old_HN_item[key]["max"]){
+                            count += 1;
+                            let anchor = age_span.getElementsByTagName("a")[0];
+                            age_span.id = "np_"+count.toString();
+                            anchor.style['color'] = '#b37400';
+                            anchor.style['font-weight'] = 'bold';
+                            post.style['background-color'] = '#e6e6e6';
 
-                        if(count < diff_count){
-                            let new_anchor = document.createElement("a");
-                            new_anchor.href = "#np_"+(count+1).toString();
-                            new_anchor.innerHTML = "next unread";
-                            age_span.innerHTML = age_span.innerHTML + " | ";
-                            age_span.appendChild(new_anchor);
-                        }
-                    }//end timestamp vs max check
-                }//end age_span exists check
-            }//end post iterator
+                            if(count < diff_count){
+                                let new_anchor = document.createElement("a");
+                                new_anchor.href = "#np_"+(count+1).toString();
+                                new_anchor.innerHTML = "next unread";
+                                age_span.innerHTML = age_span.innerHTML + " | ";
+                                age_span.appendChild(new_anchor);
+                            }
+                        }//end timestamp vs max check
+                    }//end age_span exists check
+                }//end post iterator
 
-            let anchor = get_count_anchor();
-            anchor.style['color'] = '#b37400';
-            anchor.style['font-wieght'] = 'bold';
-            let subline = get_subline_span();
-            subline.innerHTML = subline.innerHTML + " <a href='#np_1'>("+diff_count+" new)</a>";
-        }//end new-post-count vs old-post-count check
-    }//end if HN_data exists
-}).then( store_data );
+                let anchor = get_count_anchor();
+                anchor.style['color'] = '#b37400';
+                anchor.style['font-wieght'] = 'bold';
+                let subline = get_subline_span();
+                subline.innerHTML = subline.innerHTML + " <a href='#np_1'>("+diff_count+" new)</a>";
+            }//end new-post-count vs old-post-count check
+        }//end if HN_data exists
+    }).then( store_data ).then( get_HN_item ).then( clean_data );
+}
 
-/* ----
-// To-do: Implement a mechanism that cleans up old items in storage.
-//   - Probably do a check for time since last clean-up
-//     - If it has been long enough, iterate through storage items seeking if
-//     the item's associated timestamp is older than now.timestamp-4weekdiff
-//       - If so then call chrome.storage.local.remove().
----- */
+HN_nph_main();
+
+// chrome.storage.local.set({"HN_timestamp_test": {"max":1742146925,"count":0}});
+
